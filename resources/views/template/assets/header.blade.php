68e725383be88b67c65de907bb786150f0f2712b
Brandon Johnson <bluejd910@gmail.com>
2015-02-11 10:31:24 -0500

    Cleanup some assets, change gitignore to ignore assets vendor folder

--- resources/views/template/assets/header.blade.php
+++ resources/views/template/assets/header.blade.php
@@ -1,3 +1,4 @@
 <link rel="stylesheet" href="/assets/dist/css/app.min.css" type="text/css" />
+<link rel="stylesheet" href="/assets/src/css/animate.min.css" type="text/css" />
 <link rel="stylesheet" href="/assets/src/css/fonts.css" type="text/css" />
 <link rel="stylesheet" href="/assets/src/css/gluii.css" type="text/css" />
