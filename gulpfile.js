d226f1860ad443f32be9f3708ae75101f0fe4e7e
Brandon Johnson <bluejd910@gmail.com>
2015-02-20 19:42:20 -0500

    Moved assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,93 +1,122 @@
-var gulp			= require('gulp');
-var path			= require('path');
-var autoPrefixer	= require('gulp-autoprefixer');
-var clean			= require('gulp-rimraf');
-var concat			= require('gulp-concat');
-var concatCss		= require('gulp-concat-css');
-var del				= require('del');
-// var es				= require('event-stream');
-var gutil			= require('gulp-util');
-var less			= require('gulp-less');
-var minifyCss		= require('gulp-minify-css');
-var notify			= require('gulp-notify');
-var rename			= require('gulp-rename');
-var uglify			= require('gulp-uglify');
-
-//load in the plugins via the gulp-load-plugins plugin - the plugins are defined in the package.json file
-var plugins = require('gulp-load-plugins')();
+var gulp			= require('gulp'),
+	path			= require('path'),
+	del				= require('del'),
+	gutil			= require('gulp-util');
+var plugins			= require('gulp-load-plugins')();
 
 // Distribution aka the fishing line for all files
 var dir = {
-	assets:		'./resources/assets/',
 	build:		'./resources/assets/build/',
 	dist:		'./public/assets/dist/',
-	source:		'./resources/assets/src/',
+	source:		'./resources/assets/',
 	vendor:		'./resources/assets/vendor/'
 	};
 
 var config = {
 	paths: {
-		vendors: {
-			css:	[dir.vendor + 'animate.css/animate.css'],
-			js:		[dir.vendor + 'jquery/jquery.js', dir.vendor + 'bootstrap/dist/js/bootstrap.js']
-		},
-		less: {
-			src:	[
-				dir.vendor + 'bootstrap/less/bootstrap.less',
-				dir.source + 'less/app.less',
-				dir.vendor + 'simple-line-icons/less/simple-line-icons.less',
-				dir.vendor + 'font-awesome/less/font-awesome.less',
-			]
-		},
-		js: {
-			src:	[dir.source + 'js/*.js']
-		},
+
+		// Less
+		less: [
+			dir.vendor + 'bootstrap/less/bootstrap.less',
+			dir.source + 'less/app.less',
+			dir.vendor + 'ionicons/less/ionicons.less',
+		],
+
+		// Fonts
+		fontdirs: [
+			dir.vendor + 'bootstrap/fonts',
+			dir.vendor + 'ionicons/fonts/',
+		],
+
+		// CSS
+		css: [
+				// Essentials
+				dir.vendor + 'animate.css/animate.css',
+				// Plugins
+				dir.vendor + 'jcrop/css/jquery.Jcrop.css',
+				dir.vendor + 'chosen/chosen.css',
+		],
+
+		// Javascripts
+		js: [
+			// Essentials
+			dir.vendor + 'jquery/dist/jquery.min.js',
+			dir.vendor + 'modernizr/modernizr.js',
+			dir.vendor + 'bootstrap/dist/js/bootstrap.js',
+			dir.source + 'js/gluii.js',
+			dir.vendor + 'jqueryui-touch-punch/jquery.ui.touch-punch.min.js',
+			// Plugins
+			dir.vendor + 'jcrop/js/jquery.Jcrop.js',
+			dir.vendor + 'slimscroll/jquery.slimscroll.js',
+			dir.vendor + 'chosen/chosen.jquery.js',
+			dir.vendor + 'bootstrap-filestyle/src/bootstrap-filestyle.js',
+		],
+
 		build: {
-			less:	dir.build + 'css/less.css',
-			css:	dir.build + 'css/app.css',
-			js:		dir.build + 'js/app.js',
+			css: [
+				dir.build + 'css/less.css',
+				dir.build + 'css/fonts.css',
+				dir.build + 'css/styles.css',
+			],
+			js: [
+				dir.build + 'js/scripts.js',
+			],
 		}
 	},
 	autoPrefixBrowers: ['Android2.3','Android>=4','Chrome>=20','Firefox>=24','Explorer>=8','iOS>=6','Opera>=12','Safari>=6']
 };
 
+// Check files exist
+// gulp.task('filecheck', function(){
+// 	var files = glob.sync(mask);
+// 	if (!files || !files.length) {
+// 	  // handle empty list
+// 	}
+// });
+
 // Less
 gulp.task('less', ['clean'], function(){
-	return gulp.src(config.paths.less.src, {base: dir.vendor + './css'})
-		.pipe(less().on('error', gutil.log))
-		.pipe(concatCss('app1.less.css'))
+	return gulp.src(config.paths.less, {base: dir.build + 'css/'})
+		.pipe(plugins.expectFile(config.paths.less))
+		.pipe(plugins.less().on('error', gutil.log))
+		.pipe(plugins.concatCss('app1.css'))
 		.pipe(gulp.dest(dir.build + 'css/'))
-		.pipe(notify({ message: 'Less compiled' }));
+		.pipe(plugins.notify({ message: 'Less compiled' }))
+		.on('error', function (err) { gutil.log(err); });
 });
 
 // CSS Scripts
 gulp.task('css', ['less'], function(){
-	return gulp.src([dir.source + 'css/*.css', config.paths.vendors.css])
-		.pipe(concatCss('app2.css.css'))
+	return gulp.src(config.paths.css)
+		.pipe(plugins.expectFile(config.paths.css))
+		.pipe(plugins.concatCss('app-2.css'))
 		.pipe(gulp.dest(dir.build + 'css/'))
-		.pipe(notify({ message: 'CSS from source + vendors compiled' }));
+		.pipe(plugins.notify({ message: 'CSS from source + vendors compiled' }));
 });
 
 // Compile all CSS
 gulp.task('styles', ['less', 'css'], function(){
-	return gulp.src(dir.build + 'css/*.css')
-		.pipe(autoPrefixer(config.autoPrefixBrowers))
+	return gulp.src(config.paths.build.css)
+		.pipe(plugins.expectFile(config.paths.build.css))
+		.pipe(plugins.concatCss('gluii.css'))
+		.pipe(plugins.autoprefixer(config.autoPrefixBrowers))
 		.pipe(gulp.dest(dir.dist + 'css/'))
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(minifyCss())
+		.pipe(plugins.rename({ suffix: '.min' }))
+		.pipe(plugins.minifyCss())
 		.pipe(gulp.dest(dir.dist + 'css/'))
-		.pipe(notify({ message: 'All CSS in build folder compiled & minified' }));
+		.pipe(plugins.notify({ message: 'All CSS in build folder compiled & minified' }));
 });
 
 // Javascript
 gulp.task('js', function(){
 	return gulp.src(config.paths.js.src)
-		.pipe(concat('app.js'))
+		.pipe(plugins.expectFile(config.paths.js.src))
+		.pipe(plugins.concat('app.js'))
 		.pipe(gulp.dest(dir.dist + 'js/'))
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(uglify())
+		.pipe(plugins.rename({ suffix: '.min' }))
+		.pipe(plugins.uglify())
 		.pipe(gulp.dest(dir.dist + 'js/'))
-		.pipe(notify({ message: 'Javascripts compiled and minified' }));
+		.pipe(plugins.notify({ message: 'Javascripts compiled and minified' }));
 });
 
 // Clean the public dist folder
--- resources/views/template/assets/footer.blade.php
+++ resources/views/template/assets/footer.blade.php
@@ -1,6 +1,4 @@
-<script src="/assets/src/js/app.min.js" type="text/javascript"></script>
-<script src="/assets/src/js/eldarion-ajax.min.js" type="text/javascript"></script>
-<script src="/assets/src/js/gluii.js" type="text/javascript"></script>
+<script src="/assets/src/js/gluii.min.js" type="text/javascript"></script>
 
 <!-- Google Analytics -->
 <script>
