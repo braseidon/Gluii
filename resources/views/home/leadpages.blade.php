7f3d17c966ef23984ff8ec22603a5cb011686716
Brandon Johnson <bluejd910@gmail.com>
2015-03-21 14:31:41 -0400

    Added Google Analytics

--- resources/views/home/leadpages.blade.php
+++ resources/views/home/leadpages.blade.php
@@ -4,6 +4,15 @@
 		<script type="text/javascript" src="https://my.leadpages.net/template/load-141f9a846639c5-14f1e8e00639c5-6ENMfre69E7Cw2kRGO9fI60kjNj3rxNd.js"></script>
 		<meta name="leadpages-meta-id" content="141f91a73f72a2:14f1e1900b46dc" />
 		<meta name="leadpages-served-by" content="html" />
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-59763889-1', 'auto');
+			ga('send', 'pageview');
+		</script>
 	</head>
 	<body>
 	</body>
