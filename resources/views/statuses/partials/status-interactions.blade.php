055eff98cffece757affa5fa38b763330b5f096c
Brandon Johnson <bluejd910@gmail.com>
2015-03-24 14:38:54 -0400

    Another logged out fix

--- resources/views/statuses/partials/status-interactions.blade.php
+++ resources/views/statuses/partials/status-interactions.blade.php
@@ -1,31 +1,33 @@
 <ul class="list-inline m-t-sm m-b-none block">
-	<!-- like -->
-	<li>
-		@if($status->isLikedBy(Auth::getUser()))
-			<form action="{{ route('status/unlike') }}" method="POST">
-				{!! Form::hidden('status_id', $status->id) !!}
-				{!! Form::token() !!}
-				<button type="submit" class="btn btn-link btn-sm">
-					<i class="icon icon-dislike"></i> Unlike
-				</button>
-			</form>
-		@else
-			<form action="{{ route('status/like') }}" method="POST">
-				{!! Form::hidden('status_id', $status->id) !!}
-				{!! Form::token() !!}
-				<button type="submit" class="btn btn-link btn-sm">
-					<i class="icon icon-like"></i> Like
-				</button>
-			</form>
-		@endif
-	</li>
-	<!-- comment -->
+	{{-- like --}}
+	@if(Auth::check())
+		<li>
+			@if($status->isLikedBy(Auth::getUser()))
+				<form action="{{ route('status/unlike') }}" method="POST">
+					{!! Form::hidden('status_id', $status->id) !!}
+					{!! Form::token() !!}
+					<button type="submit" class="btn btn-link btn-sm">
+						<i class="icon icon-dislike"></i> Unlike
+					</button>
+				</form>
+			@else
+				<form action="{{ route('status/like') }}" method="POST">
+					{!! Form::hidden('status_id', $status->id) !!}
+					{!! Form::token() !!}
+					<button type="submit" class="btn btn-link btn-sm">
+						<i class="icon icon-like"></i> Like
+					</button>
+				</form>
+			@endif
+		</li>
+	@endif
+	{{-- comment --}}
 	<li>
 		<a href="javascript:void(0);" class="btn btn-link btn-sm">
 			<i class="icon icon-bubble"></i> Comment
 		</a>
 	</li>
-	<!-- show comments -->
+	{{-- show comments --}}
 	<li>
 		<a href="javascript:void(0);" class="btn btn-link btn-sm">
 			Show All Comments ({{ $status->present()->replyCount }})
@@ -36,7 +38,7 @@
 			ID#{{ $status->id }}
 		</a>
 	</li>
-	@if(Auth::getUser()->id == $status->author->id)
+	@if(Auth::check() && Auth::getUser()->id == $status->author->id)
 		<li class="pull-right">
 			<!-- edit -->
 			<a href="javascript:void(0);" class="btn btn-link btn-sm text-muted">Edit</a>
